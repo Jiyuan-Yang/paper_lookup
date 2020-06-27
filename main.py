2f9675592762e604bccf47e338d509a9b479c63e
Jiyuan-Yang <jim_9827@icloud.com>
2020-06-27 23:59:49 +0800

    update output

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from init import init_config
 
 cmd_parser = argparse.ArgumentParser(description='Paper Lookup, an easier way to manage your papers.')
 cmd_parser.add_argument('operation', default='', help='select an operation')
+cmd_parser.add_argument('other', nargs=argparse.REMAINDER)
 
 args = cmd_parser.parse_args()
 operation = args.operation
 
+
 if operation == 'init':
     init_config()
+elif operation == 'env':
+    pass
--- utils/output.py
+++ utils/output.py
@@ -1,22 +1,22 @@
 type_dict = {
-    'info': '📢️',
+    'info': '➡️',
     'warning': '⚠️',
     'error': '❌',
     'success': '✅',
 }
 
 type_dict_plain = {
-    'info': 'info',
-    'warning': 'warning',
-    'error': 'error',
-    'success': 'success',
+    'info': '\033[34minfo\033[0m',
+    'warning': '\033[33mwarning\033[0m',
+    'error': '\033[31merror\033[0m',
+    'success': '\033[32msuccess\033[0m',
 }
 
 notify_template = '{type} {message}'
 notify_template_plain = '[{type}] {message}'
 
 
-def notify_print(notify_type, message, use_plain=False):
+def notify_print(notify_type, message, use_plain=True):
     type_d = type_dict if not use_plain else type_dict_plain
     template = notify_template if not use_plain else notify_template_plain
     print(template.format(
